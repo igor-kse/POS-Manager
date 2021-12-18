ceefb61f47fa9848e5560e4dd4d9acc6b5a934b8
Igor Kopyshev <kopyshev.sd@gmail.com>
2021-12-18 03:06:55 +0300

    Initial commit

---
+++ src/test/java/ru/posmanager/PosmanagerApplicationTests.java
@@ -0,0 +1,12 @@
+package ru.posmanager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PosmanagerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+}
