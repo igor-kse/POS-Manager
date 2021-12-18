f2b0b88e3433d190dd810e1cb3f3961dc1c27118
Igor Kopyshev <kopyshev.sd@gmail.com>
2021-12-18 19:47:27 +0300

    implemented AuthorizedUser.java

---
+++ src/main/java/ru/posmanager/AuthorizedUser.java
@@ -0,0 +1,23 @@
+package ru.posmanager;
+
+import lombok.Getter;
+import lombok.ToString;
+import ru.posmanager.to.user.UserDTO;
+
+import java.io.Serial;
+
+@ToString
+public class AuthorizedUser extends org.springframework.security.core.userdetails.User {
+    @Serial
+    private static final long serialVersionUID = 1L;
+
+    @Getter
+    private final UserDTO userDTO;
+
+    public AuthorizedUser(UserDTO dto) {
+        super(dto.getEmail(), dto.getPassword(), dto.isEnabled(),
+                true, true, true, dto.getRoles());
+        userDTO = new UserDTO(dto);
+        userDTO.setPassword(null);
+    }
+}
